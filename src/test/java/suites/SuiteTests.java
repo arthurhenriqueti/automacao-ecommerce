325c8ee7f440f80853a06b771e11b0a1d9cf6151
Arthur Henrique <arthurbleckrep2@gmail.com>
2022-09-14 12:19:35 -0300

    corrections

--- src/test/java/suites/SuiteTests.java
+++ src/test/java/suites/SuiteTests.java
@@ -5,12 +5,14 @@ import org.junit.runners.Suite;
 import tests.ContactTests;
 import tests.LoginTests;
 import tests.SearchTests;
+import tests.SectionWomenTests;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
         LoginTests.class,
         ContactTests.class,
-        SearchTests.class
+        SearchTests.class,
+        SectionWomenTests.class
 })
 public class SuiteTests {
 }
